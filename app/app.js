c42f2884233de66431b1b3c68aa60610708f8962
cichy380 <marcin.dobroszek@gmail.com>
2022-01-15 21:07:09 +0100

    Enable CORS

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
+const {origin} = require('./config');
 const express = require('express')
 const handleSyntaxError = require('./services/handle-syntax-error')
+const cors = require("cors");
 
 const app = express()
+
+app.use(cors({origin}));
 
 // parse requests of "application/json" content-type
 app.use((req, res, next) => handleSyntaxError(express.json(), req, res, next))
--- app/config.js
+++ app/config.js
@@ -3,4 +3,5 @@ require('dotenv').config()
 module.exports = {
   port: process.env.PORT || 3000,
   database: process.env.DATABASE,
+  origin: 'https://cichy380.github.io', // only requests from this "origin" will be allowed
 }
